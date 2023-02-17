5ee093582a8812163f5d240190ae7b4ec8437ba5
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-17 04:11:41 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*Hello this is betty comment style*/
+/**
+ * main :- entry
+ * Return 0
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
