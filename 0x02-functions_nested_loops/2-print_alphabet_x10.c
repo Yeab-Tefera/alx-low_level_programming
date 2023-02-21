31de4758737c2165e91cd42289a70b8ff32f058b
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-21 14:14:49 -0800

    Printing lowercaser alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Print 10 times lowercase
+ * alphabet followed by a new line
+ *
+ * Return:
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	char b;
+
+	for (a = 'a'; a <= 'j'; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n')
+	}
+}
