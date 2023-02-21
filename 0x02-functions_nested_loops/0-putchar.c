a1e3aa2167f539a400bae19b8ed7cc0201b90eb8
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-21 12:50:06 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main: Prints _putchar followed by a new line
+ * return: 0
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+	
