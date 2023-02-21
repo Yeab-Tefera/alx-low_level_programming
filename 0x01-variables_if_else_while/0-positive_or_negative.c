979d742df834f9078d756ad810bc7255c96c379c
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-20 15:10:13 -0900

    Random number given to n and display if it is positive negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  *  main - assign a random number to the variable n each time it is executed 
@@ -22,5 +22,6 @@ int main(void)
 	       printf("%d is zero\n", n);
 	if (n < 0) 
 	       printf("%d is negative\n". n);
+
 	return (0);
 }
