749153782ac54ee832ebad1bd9082e721d9ec11a
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-21 13:37:14 -0800

    print alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- print the alphabet, in lowercase 
+ * followed by a new line
+ * use only _putchar twice 
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	putchar('\n');
+}
