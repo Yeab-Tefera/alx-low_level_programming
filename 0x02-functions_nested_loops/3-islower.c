a842e32c9d9cb0c5292e6e34676a98f31a295c60
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-21 14:39:29 -0800

    Checks for lowercase charachters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower- Check for lowercase character
+ *
+ * Return: 1 if c is lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z');
+     	{
+		return (1);
+	}
+	else
+		return (0);
+}
