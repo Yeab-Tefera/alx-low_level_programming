8c8a1e74f7e4ecdf083a690c30f3bff005930a20
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-21 16:09:32 -0800

    Last 10 digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit- Prints the last digit of a number
+ * @n: value to be printed
+ *
+ * Return: 
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+
+	if (n < 0)
+		n = -n;
+
+	m = n % 10;
+
+	if (m < 0)
+		m = -m;
+
+	_putchar(m + '0');
+
+	return (a);
+}
