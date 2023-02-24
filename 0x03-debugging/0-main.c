3a765c224d3e1452d4b27f1e42751b959bb1af57
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-24 12:28:55 -0900

    Debugging positive and negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main- Tests that the function positive_or_negative
+ * gives the correct output when given a case 0.
+ * 
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return 0;
+}
