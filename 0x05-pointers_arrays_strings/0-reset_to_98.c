4223b138fd76f6ff4e342b7b8ec411e605d0f3c3
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-02-28 12:50:57 -0900

    Pointer to change the value of n to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98- Updates the value it points to 98
- * @*n: pointer to the number
+ * @n: pointer to the number
  *
- * Return: nothing 
+ * Return: nothing
  */
 
 void reset_to_98(int *n)
