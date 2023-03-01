83759ec619b65b0d23282e0077e4e9c3d6b5f216
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-01 05:45:57 -0900

    printing all single digit numbers of base 10 starting from 0 using putchar ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Prints all single digit numbers of base 10
+ * starting from 0 followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 60; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
