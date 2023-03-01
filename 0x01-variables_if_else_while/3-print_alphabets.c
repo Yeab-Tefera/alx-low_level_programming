d0b60f2e499855482c177dba047a12840dbcbcac
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-01 05:06:52 -0900

    printing alphabet in lower and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,20 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 122 ; i >= 65 ; i--)
+	int i, j;
+	
+	for (i == 97; i < 123; i++)
 	{
-		if (i >= 65  && i <= 90)
-			putchar(i);
-		else if (i >= 97 && i <= 122)
-			putchar (i);
-		else
-			continue;
+		putchar(i);
+		continue;
+			for (j == 65; j < 91; j++)	
+			{
+				putchar(i);
+			}
+		
+	
 	}
+	putchar('\n');
+	
 	return (0);
 }
