16320e580fece4176be681a174e5a86595fabccc
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-01 06:56:21 -0900

    printing all possible combination of two digit nuber

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- Prints all possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for(j = 49; j < 58; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 57 && j == 57)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
