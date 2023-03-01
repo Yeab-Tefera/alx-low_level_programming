1049004133bc91dcf7b9ec520d847e3986ca9fe4
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-01 03:24:45 -0900

    asignning a rantom number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,15 +3,13 @@
 #include <stdio.h>
 
 /**
- * main- assigns a rendom number to the variable n
- * each time it is executed
+ * main- 
  *
  * Return: Always 0 (success)
- */
-
 int main(void)
+
 {
-	int n, x;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
