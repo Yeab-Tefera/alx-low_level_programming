bd79bbe134eb8899315aa7f2b1482a164d7ff166
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-01 07:02:41 -0900

    printing all possible combination of two digit nuber

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,11 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				if (i == 57 && j == 57)
-					continue;
-
-				putchar(',');
-				putchar(' ');
+				if (i != 57 && j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
