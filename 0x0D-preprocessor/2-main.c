3dc7fec098d3bc7d9139739f68fa0da46d4cbbd4
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-20 04:21:26 -0900

    Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- prints the name of the file
+ * it was compiled from, followed by a new line.
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("%s",__FILE__);
+}
