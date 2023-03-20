1c5d6bde157e9267021d7de25a62fa48bbb4b04b
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-20 09:19:17 -0900

    Printing the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _function_like_macro_h
 #define _function_like_macro_h
 
-#define ABS(x) (((x) < 0) ? ((x) * (-1)): (x))
+#define ABS(x) (((x) < 0) ? (-(x)): (x))
 
-#endif
+#endif /* _fuction_like_macro_h */
