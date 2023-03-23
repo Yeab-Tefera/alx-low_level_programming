bba521b804f6841e52553e9c5342eb4aa567cd70
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-23 11:50:27 -0900

    forgot to add va_end in 0 sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,19 +5,19 @@
  * sum_them_all - Returns the sum of all its parameters
  * @n: number of arguments taken
  *
- * Return: 0 if n == 0, other wise success
+ * Return: 0 if n == 0, other wise sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	unsigned int sum = 0;
 	va_list ap;
 
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	for (int i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+	for (unsigned int i = 0; i < n; i++)
+		sum += va_arg(ap,const unsigned int);
 	va_end(ap);
 
 	return (sum);
