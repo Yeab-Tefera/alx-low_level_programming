8d751afca111edbeb50d0ca175cc5cfaaa137925
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-23 11:39:55 -0900

    created a new folder 0x10_variadic_funcions and added a readme file and add variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters
+ * @n: number of arguments taken
+ *
+ * Return: 0 if n == 0, other wise success
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (int i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	return (sum);
+}
